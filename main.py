30c00adf7b45282560252c1c1f2db725a8f1b2b1
Chinmay Pant <64401853+chinmaypant21@users.noreply.github.com>
2021-10-20 18:34:33 +0000

    Meme API async fixes

--- main.py
+++ main.py
@@ -3,44 +3,63 @@ import requests
 import json
 import io
 import aiohttp
-from os import environ 
+
+from os import environ
+
 BOT_TOKEN = environ['DISCORD_DOT_TOKEN']
-link = "https://meme-api.herokuapp.com/gimme"
+nsfw_filter = True
+meme_api = "https://meme-api.herokuapp.com/gimme"
+
+
 bot = discord.Client()
-'''NSFW FILTER TODO'''
 
 def message_activities(msg):
-  activities = {
-    ".hello":1,
-    ".bye":2,
-    ".info":3
-  }
-  if msg in activities:
-    return activities[msg]
-  else:
-    return None
+    activities = {".hello": 1, ".bye": 2, ".info": 3}
+    if msg in activities:
+        return activities[msg]
+    else:
+        return None
+
 
 @bot.event
 async def on_ready():
-  print(f"Logged In as {bot.user}")
+    print(f"Logged In as {bot.user}")
 
-@bot.event
-async def on_message(msg):
-  # If message is the one which is sent by this bot
-  if(msg.author != bot.user and msg.content.startswith(".")):
-    response = requests.request("GET",link)
-    json_data = response.json()
-    img_url = json_data["url"]
+async def meme(msg):
+  response = requests.request("GET",meme_api)
+  json_data = response.json()
+  img_url = json_data["url"]
+  if nsfw_filter and json_data['nsfw']:
+    print("NSFW Found!")
+    return await meme(msg)
 
-    async with aiohttp.ClientSession() as session:
-      async with session.get(img_url) as resp:
+  async with aiohttp.ClientSession() as session:
+    async with session.get(img_url) as resp:
         if resp.status != 200:
             print('Could not download file...')
             return
         data = io.BytesIO(await resp.read())
-        await msg.channel.send(file=discord.File(data, 'image.png'))
-    # reply = message_activities(msg.content)
-    # if reply is not None:
-    #   await msg.channel.send(reply)
-    
+  await msg.channel.send(file=discord.File(data, 'meme.png'))
+
+async def greet(msg):
+  # await msg.channel.send("")
+  pass
+
+@bot.event
+async def on_message(msg):
+    # If message is the one which is sent by this bot
+    if (msg.author != bot.user and msg.content.startswith(".")):
+      if(msg.content == ".meme"):
+        await meme(msg)
+      elif msg.content == '.hi':
+        await greet(msg)
+      else:
+        pass
+        
+      # await msg.channel.send(file=discord.File(await meme(get_meme), 'meme.png'))
+        # reply = message_activities(msg.content)
+        # if reply is not None:
+        #   await msg.channel.send(reply)
+
+
 bot.run(BOT_TOKEN)
